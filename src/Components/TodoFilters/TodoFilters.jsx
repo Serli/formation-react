cccb43a728db5ff7ca7980eafb3262d7c2643dd2
Mathieu Fort <mathieu.fort86@gmail.com>
2023-10-16 22:52:48 +0200

    update status works

--- src/Components/TodoFilters/TodoFilters.jsx
+++ src/Components/TodoFilters/TodoFilters.jsx
@@ -1,7 +1,39 @@
+import {Status} from "../../Model/StatusEnum.js";
+import "./TodoFilters.css";
 export const TodoFilters = () => {
     return (
-        <div>
+        <>
             <h2>Filtres</h2>
-        </div>
+            <div className="filter-grid">
+                <div className="left-side-filter" >
+                    <h3>Par état</h3>
+                    <fieldset>
+                        <div className="check-element">
+                            <input type="checkbox" id="todoStatus" name="todoStatus"/>
+                            <label htmlFor="todoStatus">{Status.todo}</label>
+                        </div>
+                        <div className="check-element">
+                            <input type="checkbox" id="inProgressStatus" name="inProgressStatus" />
+                            <label htmlFor="inProgressStatus">{Status.inProgress}</label>
+                        </div>
+                        <div className="check-element">
+                            <input type="checkbox" id="doneStatus" name="doneStatus" />
+                            <label htmlFor="doneStatus">{Status.done}</label>
+                        </div>
+                    </fieldset>
+                </div>
+                <div className="right-side-filter">
+                    <h3>Par date</h3>
+                    <div>
+                        <p>
+                            <span className="filter-date">du</span>
+                            <input className="filter-date" type="date" id="fromDate" name="fromDate"/>
+                            <span className="filter-date">au</span>
+                            <input className="filter-date" type="date" id="toDate" name="toDate"/>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
--- src/Components/TodoList/Todo/Todo.jsx
+++ src/Components/TodoList/Todo/Todo.jsx
@@ -1,7 +1,19 @@
 /* eslint-disable react/prop-types */
 import "./Todo.css";
+import {Status} from "../../../Model/StatusEnum.js";
+import {useState} from "react";
 
 export const Todo = (props) => {
+
+    // eslint-disable-next-line no-unused-vars
+    const [ status,setStatus] = useState('');
+
+    const handleStatusClick = () => {
+        const status = props.todo.status === Status.todo ? Status.inProgress : Status.done
+        props.todo.status = status;
+        setStatus(status);
+    }
+
     return (
         <div className="task">
             <div className="leftSide">
@@ -10,7 +22,7 @@ export const Todo = (props) => {
             </div>
             <div className="rightSide">
                 <p className="date">{props.todo.createdAt}</p>
-                <p className="status">{props.todo.status}</p>
+                <p className="status" onClick={handleStatusClick}>{props.todo.status}</p>
             </div>
         </div>
     );
